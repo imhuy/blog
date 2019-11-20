cb65caa92cdea9f8143c82c13a7009bf1f5868fc
Huy Lưu <huylv@hopnhat.vn>
2019-11-20 08:25:49 +0700

    Thêm git ignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>  
+                <Link to="/feed">Home</Link>
+          <Switch>
+            <Route path="/feed" component={Header} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
-
+ 
 export default App;
