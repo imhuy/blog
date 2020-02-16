76fa1f697c133bc9b381c5891c1b9ecceaa0a4c3
Huy <>
2020-02-17 05:45:50 +0700

    add homepage

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,11 +1,119 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
 import React, { Component } from 'react';
-
+import useStyles from './styles';
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 class HomePage extends Component {
+
+    Copyright() {
+        return (
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © '}
+                <Link color="inherit" href="https://material-ui.com/">
+                    Your Website
+            </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        );
+    }
+    
     render() {
         return (
-            <div>
-                <p>Test </p>
-            </div>
+            <React.Fragment>
+                <CssBaseline />
+                <AppBar position="relative">
+                    <Toolbar>
+                        <CameraIcon className={useStyles.icon} />
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Album layout
+                </Typography>
+                    </Toolbar>
+                </AppBar>
+                <main>
+                    {/* Hero unit */}
+                    <div className={useStyles.heroContent}>
+                        <Container maxWidth="sm">
+                            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                                Album layout
+                  </Typography>
+                            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                                Something short and leading about the collection below—its contents, the creator, etc.
+                                Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                                entirely.
+                  </Typography>
+                            <div className={useStyles.heroButtons}>
+                                <Grid container spacing={2} justify="center">
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">
+                                            Main call to action
+                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="outlined" color="primary">
+                                            Secondary action
+                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Container>
+                    </div>
+                    <Container className={useStyles.cardGrid} maxWidth="md">
+                        {/* End hero unit */}
+                        <Grid container spacing={4}>
+                            {cards.map(card => (
+                                <Grid item key={card} xs={12} sm={6} md={4}>
+                                    <Card className={useStyles.card}>
+                                        <CardMedia
+                                            className={useStyles.cardMedia}
+                                            image="https://source.unsplash.com/random"
+                                            title="Image title"
+                                        />
+                                        <CardContent className={useStyles.cardContent}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                Heading
+                          </Typography>
+                                            <Typography>
+                                                This is a media card. You can use this section to describe the content.
+                          </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small" color="primary">
+                                                View
+                          </Button>
+                                            <Button size="small" color="primary">
+                                                Edit
+                          </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </main>
+                {/* Footer */}
+                <footer className={useStyles.footer}>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Footer
+              </Typography>
+                    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                        Something here to give the footer a purpose!
+              </Typography>
+
+                </footer>
+                {/* End footer */}
+            </React.Fragment>
         );
     }
 }
---
+++ client/src/components/HomePage/styles.js
@@ -0,0 +1,32 @@
+
+export default {
+    icon: {
+        marginRight: 2
+    },
+    heroContent: {
+        backgroundColor: '#fff',
+        padding: 10
+    },
+    heroButtons: {
+        marginTop: 4
+    },
+    cardGrid: {
+        paddingTop: 8,
+        paddingBottom: 8
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: '#fff',
+        padding: 6
+    },
+};
